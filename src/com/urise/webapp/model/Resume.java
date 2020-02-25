a1560e871aa1b64340d652d8a52cc0943be8e044
Vladimir Semenovich <vladimirgdsr@gmail.com>
2020-02-25 13:14:35 -0800

    implementation of the interface

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -9,15 +9,15 @@ public class Resume {
 
     // Unique identifier
     private String uuid;
-    
+
     public String getUuid() {
         return uuid;
     }
-    
+
     public void setUuid(String uuid) {
         this.uuid = uuid;
     }
-    
+
     @Override
     public String toString() {
         return uuid;
